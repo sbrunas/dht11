811975795b5bfd7a8099248acfbab34d7e0be251
Sebastian Bruna <seb.bruna@gmail.com>
2019-09-04 22:19:44 -0400

    dht_log changesy

--- dht_log.py
+++ dht_log.py
@@ -1,43 +1,42 @@
 #!/usr/bin/python
 
-# Importa las librerias necesarias 
+#--------------------------------IMPORT LIBRARIES--------------------------------
+import sys
 import time
-import datetime
 import Adafruit_DHT
 
-# Log file
+#--------------------------------PATH LOG FILE--------------------------------
 log_path = "/home/pi/log/iot/"
 
-# Configuracion del tipo de sensor DHT
+#--------------------------------CONF TYPE OF SENSOR--------------------------------
 sensor = Adafruit_DHT.DHT11
 
-# Configuracion del puerto GPIO al cual esta conectado (GPIO 23)
+#--------------------------------CONFIG GPIO 23 FOR DHT11 DATA--------------------------------
 pin = 23
 
-# Escribe un archivo log en log_path con el nombre en el formato yyyy-mm-dd_dht.log
+#--------------------------------WRITE THE LOG FILE WITH THE NAME yyyy-mm-dd_dht.log--------------------------------
 def write_log(text):
 	log = open(log_path + datetime.datetime.now().strftime("%Y-%m-%d") + "_dht.log","a")
 	line = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + text + "\n"
 	log.write(line)
 	log.close()
+#--------------------------------PROGRAM--------------------------------
+try:	
 
-# Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
-try:
-	# Ciclo principal infinito
 	while True:
-		# Obtiene la humedad y la temperatura desde el sensor 
-		humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 
-		# Si obtiene una lectura del sensor la registra en el archivo log
-		if humedad is not None and temperatura is not None:
-			write_log("DHT Sensor - Temperatura: %s" % str(temperatura))
-			write_log("DHT Sensor - Humedad:  %s" % str(humedad))
+		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+		#IF WE CAN READ THE SENSOR, WRITE THE LOG FILE
+		if humidity is not None and temperature is not None:
+			write_log("DHT Sensor - temperature in ºC: %s" % str(temperature))
+			write_log("DHT Sensor - humidity in %:  %s" % str(humidity))
 		else:
-			write_log('Error al obtener la lectura del sensor')
+			write_log("Can't get data from the sensor")
 
-		# Duerme 10 segundos
+		#DELAY
 		time.sleep(1)
 
-# Se ejecuta en caso de que falle alguna instruccion dentro del try
+#WRITE ERROR TO LOG FILE 
 except Exception as e:
 	write_log(str(e))
