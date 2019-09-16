4af0f60a3a06df7e7da13c161c1d46dc847fc4f9
Sebastian Bruna <seb.bruna@gmail.com>
2019-09-16 11:38:28 -0300

    Modificaciones 16 de Septiembre, -Arreglo de variables y errores indicados por el interprete -programa revisado una vez -revisado funciones de ftp

--- dht_log_and_FTP.py
+++ dht_log_and_FTP.py
@@ -11,7 +11,7 @@
 #					- os
 #					- ftplib
 #
-#--------------------------------IMPORT LIBRARIES-----------------------------------------------------------------------
+# --------------------------------IMPORT LIBRARIES----------------------------------------------------------------------
 import sys
 import time
 import Adafruit_DHT
@@ -20,105 +20,117 @@ from ftplib import FTP
 import os
 import fileinput
  
-#--------------------------------CONNECT FTP SERVER---------------------------------------------------------------------
+# --------------------------------CONNECT FTP SERVER--------------------------------------------------------------------
 ftp = FTP()
 ftp.set_debuglevel(2)
 ftp.connect('201.215.167.63', 21) 
-ftp.login('pi','_4oYiEmqVUFl')
-ftp.cwd('path to the T & H folder')
+ftp.login('pi', '_4oYiEmqVUFl')
+ftp.cwd("/home/pi/nextcloud/data/__groupfolders/1/log_T_H/")
 
-#--------------------------------PATH LOG FILE--------------------------------------------------------------------------
+# --------------------------------PATH LOG FILE-------------------------------------------------------------------------
 log_path = "/home/pi/log/iot/"
+server_path = "/home/pi/nextcloud/data/__groupfolders/1/log_T_H/"
 lastlist = []
-localdir = "home\pi\log\iot"
 file_name = ""
-#--------------------------------CONF TYPE OF SENSOR--------------------------------------------------------------------
+date_now = ""
+date_count = 0
+# --------------------------------CONF TYPE OF SENSOR-------------------------------------------------------------------
 sensor = Adafruit_DHT.DHT11
 
-#--------------------------------CONFIG GPIO 23 FOR DHT11 DATA----------------------------------------------------------
+# --------------------------------CONFIG GPIO 23 FOR DHT11 DATA---------------------------------------------------------
 pin = 23
 
-#--------------------------------WRITE THE LOG FILE WITH THE NAME yyyy-mm-dd_dht.log------------------------------------
+# --------------------------------WRITE THE LOG FILE WITH THE NAME yyyy-mm-dd_dht.log-----------------------------------
 def write_log(text):
 	"""
 	Function to write the actual temperature in C and humidity in %
 	:param text: get in to the string to write
 	:return: nothing
+
 	"""
-	log = open(log_path + file_name,"a")
+	log = open(log_path + file_name, "a")  	# Open /home/pi/log/iot/ with file_name
 
 	# EJ: 2019-09-10 11:00:00 Temperatura=22.0*  Humedad=60.0%
 	line = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + " " + text + "\n"
 	log.write(line)
 	log.close()
 
-#--------------------------------SEND FILE------------------------------------------------------------------------------
+# --------------------------------SEND FILE-----------------------------------------------------------------------------
 def ftp_upload(localfile, remotefile):
 	"""
 	:param localfile: 
 	:param remotefile: 
-	:return: 
-	"""""
-	fp = open(localfile, 'rb')
-	#ftp.storbinary('STOR myfile.txt'.encode('utf-8'), open('myfile.txt'))
+	:return: print "after upload + localfile to remotefile"
+	This function take the path + file_name and open in "fp" and uppload the file via FTP
+	Finally close the FTP connection
+
+	"""
+	fp = open(localfile, 'rb')  # r+ is for read & write, rb read binary file
+
+	# ftp.storbinary('STOR myfile.txt'.encode('utf-8'), open('myfile.txt'))
 	ftp.storbinary('STOR %s' % os.path.basename(localfile), fp, 1024)
 	fp.close()
-	print ("after upload " + localfile + " to " + remotefile)
+	print("after upload " + localfile + " to " + remotefile)
 
-#--------------------------------SEND IMG-------------------------------------------------------------------------------
-def upload_img(file):
+# --------------------------------SEND IMG------------------------------------------------------------------------------
+def upload_file(file):
 	"""
-	:param file:
-	:return:
-	"""
-	ftp_upload(localdir + "\\" + file, file)
+	:param file: name of the current file to be upload
+	:return: None
+	This function call the ftp_upload function and concat the local path + name of the current file to be upload.
 
-#--------------------------------PROGRAM--------------------------------------------------------------------------------
-try:	
+	"""
+	ftp_upload(log_path + file, file)
+
+# --------------------------------PROGRAM-------------------------------------------------------------------------------
+try:
 
 	while True:
 
-		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)	#Read the humidity and temperature pin = GPIO 23
+		humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)   # Read the humidity and temperature pin = GPIO 23
 
-#Quede aca escribir el if si file_name es disntinto de la fecha de hoy
-		if file_name == "":
-			file_name = str(datetime.datetime.now().strftime("%Y-%m-%d") + "_dht.log")
+# ----------------------------Check the date and the file name----------------------------------------------------------
+		if date_now != str(datetime.datetime.now().strftime("%Y-%m-%d")):
+
+			date_now = str(datetime.datetime.now().strftime("%Y-%m-%d")) # save the actual day
+			file_name = date_now + "_dht.log"							 # save the actual file name
+			date_count += 1												 # add one to the day count
+
 		else:
-			file_name = datetime.datetime.now().strftime("%Y-%m-%d") + "_dht.log"
 
+			date_now = str(datetime.datetime.now().strftime("%Y-%m-%d"))
+			file_name = date_now + "_dht.log"
 
-
-		#IF WE CAN READ THE SENSOR, WRITE THE LOG FILE
+# ------------------------Check if we can read the sensor, write the log file ------------------------------------------
 		if humidity is not None and temperature is not None:
 			write_log("DHT Sensor - temperature in ºC: %s" % str(temperature)) # write temperature
 			write_log("DHT Sensor - humidity in %:  %s" % str(humidity))	   # write humidity
 		else:
-			write_log("Can't get data from the sensor") #write to the log file error in the sensor.
+			write_log("Can't get data from the sensor") 					#write to the log file error in the sensor.
 
-		# wait 10 seconds
-		time.sleep(1)
+		time.sleep(1)														   # wait 1 second
 # --------------------------------Check the hour------------------------------------------------------------------------
 		if datetime.datetime.now().strftime("%H-%M-%S") == "15:30:00":
 
-			for line in fileinput.input(localdir + file_name):
-				lastlist.append(line.rstrip("\n"))
+			for line in fileinput.input(log_path + file_name):
+				lastlist.append(line.rstrip("\n"))					# append the current text to "lastlist"
 
-			currentlist = os.listdir(localdir)
+			currentlist = os.listdir(log_path)					# save the current files in the directory tu current list
 
 			newfiles = list(set(currentlist) - set(lastlist))
 
 			if len(newfiles) == 0:
-				print "No files need to upload"
+				print("No files need to upload")
 			else:
 				for needupload in newfiles:
-					print "uploading " + localdir + "\\" + needupload
-					upload_img(needupload)
-					with open(localdir + "\\list.txt", "a") as myfile:
-					myfile.write(needupload + "\n")
+					print("uploading " + log_path + file_name + needupload)
+					upload_file(needupload)
+					with open(log_path + file_name, "a") as myfile:
+						myfile.write(needupload + "\n")
 
 			ftp.quit()
 
+# --------------------------------Write error to log file --------------------------------------------------------------
 
-#WRITE ERROR TO LOG FILE 
 except Exception as e:
 	write_log(str(e))
