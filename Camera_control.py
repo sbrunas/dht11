bd4a3f4164c7619c9a8471d5c3d9c03dc5fe9ede
Sebastian Bruna <seb.bruna@gmail.com>
2019-09-23 10:38:56 -0300

    hour test with print debug

--- Camera_control.py
+++ Camera_control.py
@@ -26,8 +26,8 @@ GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)  # Set pin 17 to be an output pin and
 # --------------------------------DATE VAR------------------------------------------------------------------------------
 date_now = ""
 date_count = 0
-check_hour_on = '10:36:00'
-check_hour_off = '10:38:00'
+check_hour_on = '10:40:00'
+check_hour_off = '10:42:00'
 
 # -create a secure connection with gmail SMTP server using SMTP_SSL() of smtplib to initiate a TLS-encrypted connecton--
 
